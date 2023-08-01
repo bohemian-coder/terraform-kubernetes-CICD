d30dcecd2f9c5bbfca51ab46618c0fba540ab620
bohemian-coder <jenniferejeme@gmail.com>
2023-08-01 11:06:19 -0500

    jenkins node setup

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -11,6 +11,14 @@ resource "aws_security_group" "demo_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+    ingress {
+    description = "Jenkins access port"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
