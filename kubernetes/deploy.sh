98202504daa6d057b89bcc0e69dd224c29f31df1
bohemian-coder <jenniferejeme@gmail.com>
2023-08-04 11:10:01 -0500

    add exec permissions to deploy script

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-kubectl apply -f namespace.yaml
-kubectl apply -f secret.yaml
-kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
+kubectl apply -f namespace.yml
+kubectl apply -f secret.yml
+kubectl apply -f deployment.yml
+kubectl apply -f service.yml
 
 #aws eks update-kubeconfig --region us-east-1 --name tkcicd-eks-01
